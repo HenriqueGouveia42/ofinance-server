c74c3a5df84f07e3bfefaaf8f0a9234012925617
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-04 18:18:40 -0300

    O token recebido pelo middleWre não vem mais do req.headers, mas sim do req.cookies.acess_token, e comentarios acerca disso

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,17 +1,15 @@
 const {verifyToken} = require('../services/authService');
 
-//A função authMiddleware é projetada para verificar a autenticidade do token, e, em seguida, permitir que a requisição
-//continue para o proximo middleware ou controller.
-//O middleware funciona como uma "ponte" entre o client e o server
-//client - middleware - server
-//nessa caso: /localhost:5000/transaction -> authMiddleware -> transactionRoutes
+// O authMiddleware verifica se o token JWT armazenado no cookie é válido antes de permitir o acesso à rota protegida.
+// Funciona como uma "ponte" entre o client e o server.
+// Exemplo de fluxo: /localhost:5000/transaction -> authMiddleware -> transactionRoutes
 
 const authMiddleware = (req, res, next) =>{
     //req.headers.authorization obtem o valor do cabecalho 'Authorization' da requisição HTTP, através do qual o client envia o token JWT
     //.slipt('') divide a string em um array utilizando o espaco como elemento delimitador.
     //Nesse exemplo, a string sera dividida em ["Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."]
     //[1] acessa o elemento de index = 1 do array, que é justamente o token JWT
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.cookies.access_token;
 
     if(!token){
         return res.status(401).json({message: "Acesso negado. Token não fornecido"});
