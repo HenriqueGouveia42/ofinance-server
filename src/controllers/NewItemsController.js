9f0f6cad6f27ac9bcacb28f86c9a30f0446ed954
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-04 18:02:54 -0300

    Prototipo inicial do controlador que cria uma nova conta para o usuario

--- src/controllers/NewItemsController.js
+++ src/controllers/NewItemsController.js
@@ -1,16 +1,29 @@
 const {prisma} = require('../config/prismaClient');
 
 
+//const newAccountController = async(req, res) =>{
+//    try{
+//        const {accountName} = req.body;
+//        if(!accountName){
+//            return res.status(400).json({error: "Nome da conta é obrigatorio!"})
+//        }
+//        const userId = req.user.id;
+//    }catch(error){
+//        console.error("Erro ao criar nova conta ", error);
+//        console.log("Erro ao criar nova conta")
+//    }
+//}
+
 const newAccountController = async(req, res) =>{
     try{
         const {accountName} = req.body;
-        if(!accountName){
-            return res.status(400).json({error: "Nome da conta é obrigatorio!"})
+        if(!accountName || typeof accountName != "string" ){
+            return res.status(400).json({error: "Nome da conta nulo ou não é uma string!"})
         }
-        const userId = req.user.id;
+        return res.status(201).json({message: "Valor recebido é uma string! Sucesso!"})
     }catch(error){
-        console.error("Erro ao criar nova conta ", error);
-        console.log("Erro ao criar nova conta")
+        console.error("Erro ao criar nova conta: ", error);
+        return res.status(500).json({ error: "Erro interno ao criar nova conta" });
     }
 }
 module.exports = {newAccountController};
