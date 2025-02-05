e4d0255eddd7f07143deb753135040b7969034d0
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-05 19:58:07 -0300

    Aprimoramento do nome da funcao para ficar mais condinzente com seu resultado. Alguns comentarios e retorno do objeto user

--- src/services/authService.js
+++ src/services/authService.js
@@ -10,15 +10,15 @@ const generateToken = (user) =>{
     return jwt.sign(payLoad, process.env.JWT_SECRET, {expiresIn: '1h'});
 }
 
-//Verificar o token JWT
-const verifyToken = (token) =>{
+//Retorna os dados decodificados do usuario a partir do token JWT enviado pelos cookies
+const getUserFromToken = (token) =>{
     try{
-        return jwt.verify(token, process.env.JWT_SECRET); //Verifica o token usando a mesma chave secreta. Se for valido, retorna os dados DECODIFICADOS DO TOKEN
+        decoded = jwt.verify(token, process.env.JWT_SECRET); //Verifica o token usando a mesma chave secreta. Se for valido, retorna os dados DECODIFICADOS DO TOKEN
+        return decoded;
     }catch (error){
-        console.error('Erro ao verificar token.', error);
+        console.error('Erro no authService ao validar o token ou retornar dados decodificados do usuario presentes no token JWT.', error);
         return null;
     }
 }
 
-
-module.exports = {generateToken, verifyToken}
+module.exports = {generateToken, getUserFromToken}
