add822d011b4b6b75c2fdab625e4525f6d234e77
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-05 19:50:19 -0300

    Eliminacao de comentarios que nao antigos que nao fazem mais sentido e utilizacao do 'jwt.verify' ao inves da funcao 'verifyToken'

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,27 +1,24 @@
-const {verifyToken} = require('../services/authService');
+const jwt = require("jsonwebtoken");
 
 // O authMiddleware verifica se o token JWT armazenado no cookie é válido antes de permitir o acesso à rota protegida.
 // Funciona como uma "ponte" entre o client e o server.
 // Exemplo de fluxo: /localhost:5000/transaction -> authMiddleware -> transactionRoutes
 
 const authMiddleware = (req, res, next) =>{
-    //req.headers.authorization obtem o valor do cabecalho 'Authorization' da requisição HTTP, através do qual o client envia o token JWT
-    //.slipt('') divide a string em um array utilizando o espaco como elemento delimitador.
-    //Nesse exemplo, a string sera dividida em ["Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."]
-    //[1] acessa o elemento de index = 1 do array, que é justamente o token JWT
+    
     const token = req.cookies.access_token;
 
     if(!token){
         return res.status(401).json({message: "Acesso negado. Token não fornecido"});
     }
 
-    const decoded = verifyToken(token);
+    const validToken = jwt.verify(token, process.env.JWT_SECRET);
     
-    if(!decoded){
-        return res.status(401).json({message: "Acesso negado. Token invalido"});
+    if(!validToken){
+        return res.status(401).json({message: "Token invalido ou expirado"});
     }
 
-    req.user = decoded; //Anexa os dados do usuario ao objeto de requisição
+    req.user = validToken; //Anexa os dados do usuario ao objeto de requisição
     
     //Chama o proximo middleware ou controller na cadeia de execucao
     next(); 
