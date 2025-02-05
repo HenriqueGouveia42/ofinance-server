2808284e1c4882edfaf3d2291e1c767858fedd0d
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-05 19:43:27 -0300

    Implementacao da rota que retorna os dados decodificados do objeto 'user' a partir do token JWT enviado através dos cookies do client

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const dotenv = require('dotenv');
 const signUpRoutes = require('./src/routes/signUpRoutes');
 const loginRoutes = require('./src/routes/loginRoutes');
 const transactionRoutes = require('./src/routes/transactionsRoutes');
-const authMiddleware = require('./src/middlewares/authMiddleware');
 const createItemsRoutes = require('./src/routes/createItemsRoutes');
+const decodedRoutes = require('./src/routes/decodedRoutes');
+const authMiddleware = require('./src/middlewares/authMiddleware');
+
 
 
 
@@ -29,6 +31,7 @@ app.use('/signup', signUpRoutes);
 app.use('/login', loginRoutes);
 app.use('/transaction', authMiddleware, transactionRoutes); //Client -> POST(/transaction/create) -> authMiddleware -> transactionController -> Banco de dados/servicos externos -> response(JSON) -> Client
 app.use('/create', authMiddleware, createItemsRoutes);
+app.use('/get-user', decodedRoutes);
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 5000;
