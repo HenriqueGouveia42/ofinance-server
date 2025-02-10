80cd1010bb98505d4db24cf83dcc032c46ac84a9
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-10 16:37:15 -0300

    Rotas com nomes pouco explicativos

--- src/routes/createItemsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {newAccountController} = require('../controllers/NewItemsController')
-
-router.post('/account', newAccountController);
-
-module.exports = router;
--- src/routes/decodedRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {getUserFromToken} = require('../controllers/authController');
-
-
-router.get('/', getUserFromToken);
-
-module.exports = router;
