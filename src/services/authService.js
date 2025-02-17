c34d830f277f7b042b8d772cdf653463d128ae31
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-17 10:30:29 -0300

    Eliiminacao de funcao que nao faz sentido

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,19 +6,9 @@ const generateToken = (user) =>{
         id: user.id,
         name: user.name,
         email: user.email,
+        defaultCurrencyId: user.defaultCurrencyId
     }
     return jwt.sign(payLoad, process.env.JWT_SECRET, {expiresIn: '1h'});
 }
 
-//Retorna os dados decodificados do usuario a partir do token JWT enviado pelos cookies
-const getUserFromToken = (token) =>{
-    try{
-        decoded = jwt.verify(token, process.env.JWT_SECRET); //Verifica o token usando a mesma chave secreta. Se for valido, retorna os dados DECODIFICADOS DO TOKEN
-        return decoded;
-    }catch (error){
-        console.error('Erro no authService ao validar o token ou retornar dados decodificados do usuario presentes no token JWT.', error);
-        return null;
-    }
-}
-
-module.exports = {generateToken, getUserFromToken}
+module.exports = {generateToken}
