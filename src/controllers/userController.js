57643bab8e4cdd0c8beef2c8cd0ef02b21ff1383
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-20 16:48:09 -0300

    Implementacao da funcao que busca os dados registrados (Nome, email, moedas criadas, id da moeda padrao, contas e categorias) de um determinado usuario

---
+++ src/controllers/userController.js
@@ -0,0 +1,51 @@
+const {prisma} = require('../config/prismaClient');
+
+const {getDefaultCurrencyId, getAccountsByUserId, getCategoriesById, getCurrenciesByUserId} = require('../services/userService');
+
+const getUserData = async(req, res) =>{
+    try{
+        const userId = req.user.id;
+
+        const userData = {
+            name: null,
+            email: null,
+            currencies: null,
+            defaultCurrencyId: null,
+            accounts: null,
+            categories: null
+        };
+        
+        const nameAndEmail = await prisma.users.findUnique({
+            where:{id: userId},
+            select: {name:true, email: true}
+        });
+
+        if(!nameAndEmail){
+            return res.status(404).json({message: "Usuario nao encontrado"});
+        }
+
+        const defaultCurrencyId = await getDefaultCurrencyId(userId);
+
+        const accounts = await getAccountsByUserId(userId);
+
+        const categories = await getCategoriesById(userId);
+
+        const currencies = await getCurrenciesByUserId(userId);
+
+        userData.name = nameAndEmail.name;
+        userData.email = nameAndEmail.email;
+        userData.currencies = currencies;
+        userData.defaultCurrencyId = defaultCurrencyId;
+        userData.accounts = accounts;
+        userData.categories = categories;
+
+        return res.status(201).json(userData);
+    }catch(error){
+        console.error("Erro ao buscar os dados cadastrados do usuario (Contas, moedas e categorias de receitas e despesas): ", error);
+        return res.status(404).json({message: "Erro ao buscar os dados cadastrados do usuario"});
+    }
+}
+
+module.exports={
+    getUserData
+}
