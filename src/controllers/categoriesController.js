a4b7e2a7c87ded961e91a97da06ebbc5b2362a04
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-20 16:06:25 -0300

    Implementacao da chamada do servico que verifica se uma determinada categoria, de um determinado tipo, já existe para um determinado usuario. Alguns comentarios. A funcao nao retorna mais o JSON da categoria criada, limitando-se tão somente a retornar o codigo de sucesso

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,15 +1,23 @@
-const { createCategory } = require("../services/categoryService");
+const { createCategory, checkIfCategoryAlreadyExists } = require("../services/categoryService");
 
 const newCategory = async(req, res) =>{
     try{
         const {name, type} = req.body;
 
+        //Verifica se o tipo é válido
         if(!['revenue', 'expense'].includes(type)){
             return res.status(400).json({message: "Tipo de categoria invalido"})
         }
 
+        //Verifica se o nome é valido
         if(typeof name !== "string" || !/^[a-zA-Z]/.test(name)){
             return res.status(400).json({message: "Nome invalido. Deve ser uma string e comecar com uma letra"})
+        }
+
+        const categoryAlreadyExists = await checkIfCategoryAlreadyExists(name, req.user.id, type);
+
+        if(categoryAlreadyExists){
+            return res.status(400).json({message: 'Usuario já tem uma categoria de receita ou despesa com esse nome!'});
         }
 
         const category = await createCategory(name, type, req.user.id);
@@ -18,7 +26,7 @@ const newCategory = async(req, res) =>{
             return res.status(400).json({message: "Erro na criação da categoria"});
         }
 
-        return res.status(201).json({message: "Categoria criada com sucesso!", category})
+        return res.status(201).json({message: "Categoria criada com sucesso!"});
 
     }catch(error){
         console.error("Erro ao criar nova categoria");
