7ee7a22f23c4cb527c833407b0cdb5ff0b10363b
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-22 14:38:49 -0300

    Implementacao da rota que chama o controller que deleta uma moeda especifica

--- src/routes/currencyRoutes.js
+++ src/routes/currencyRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {newCurrency, updateDefaultCurrency} = require('../controllers/currencyController');
+const {newCurrency, updateDefaultCurrency, deleteCurrency} = require('../controllers/currencyController');
 
 router.post('/create-currency', newCurrency);
 router.patch('/update-default-currency', updateDefaultCurrency);
+router.delete('/delete-currency', deleteCurrency)
 
 module.exports = router;
