c3c1bc07fe752b1fe30f296bf2f85f6c32f907c5
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-22 14:37:46 -0300

    Implementacao da rota que chama o controller que deleta uma conta especifica

--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createAccount, updateBalance, getAccounts} = require('../controllers/accountsController')
+const {createAccount, updateBalance, getAccounts, deleteAccount} = require('../controllers/accountsController')
 
 router.post('/create-account', createAccount);
 router.get('/get-accounts', getAccounts);
-router.patch('/update-balance', updateBalance)
+router.patch('/update-balance', updateBalance);
+router.delete('/delete-account', deleteAccount);
 
 module.exports = router;
