02d8a43c5d709753ad7db6588bcb03c2604bcf4d
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-22 14:40:27 -0300

    Implementacao da rota que chama a funcao que pega todas as transacoes feitas por um determinado usuario

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {createTransaction, readMonthTransactions, readUnpaidTransactions} = require('../controllers/transactionController');
+const {createTransaction, readMonthTransactions, readUnpaidTransactions, getAllTranscation} = require('../controllers/transactionController');
 const router = express.Router();
 
 router.post('/create-transaction', createTransaction);
 router.get('/readMonthTransaction', readMonthTransactions);
 router.get('/readUnpaidTransactions', readUnpaidTransactions)
+router.get('/get-all-transactions', getAllTranscation)
 
 module.exports = router;
