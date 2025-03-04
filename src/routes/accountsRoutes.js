228d6c0a29ffdf360d82ed577260f7693774bd2b
Henrique Gouveia <henriquesgo42@gmail.com>
2025-03-04 12:15:22 -0300

    Implementacao da rota que chama o controller que renomeia uma determinada conta de um determinado usuario

--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createAccount, updateBalance, getAccounts, deleteAccount} = require('../controllers/accountsController')
+const {createAccount, updateBalance, getAccounts, deleteAccount, renameAccount} = require('../controllers/accountsController')
 
 router.post('/create-account', createAccount);
 router.get('/get-accounts', getAccounts);
 router.patch('/update-balance', updateBalance);
+router.patch('/rename-account', renameAccount )
 router.delete('/delete-account', deleteAccount);
 
 module.exports = router;
