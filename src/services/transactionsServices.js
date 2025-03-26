a89ee839e744d69426793998460a3fc54ff23b5b
Henrique Gouveia <henriquesgo42@gmail.com>
2025-03-26 19:08:09 -0300

    Modulo de servicos relacionados a transacoes

---
+++ src/services/transactionsServices.js
@@ -0,0 +1,94 @@
+const {prisma} = require('../config/prismaClient');
+
+const newTransaction = async(
+    amount,
+    type,
+    paid_out,
+    payDay,
+    description,
+    attachment,
+    fixed,
+    repeat,
+    typeRepeat,
+    remindMe,
+    createdAt,
+    updatedAt,
+    userId,
+    categoryId,
+    accountId,
+    currencyId,
+) => {
+    try{
+        const transaction = await prisma.transactions.create({
+            data:{
+                amount,
+                type,
+                paid_out,
+                payDay,
+                description,
+                attachment,
+                fixed,
+                repeat,
+                typeRepeat,
+                remindMe,
+                createdAt,
+                updatedAt,
+                userId,
+                categoryId,
+                accountId,
+                currencyId
+            }
+        })
+        return true;
+    }catch(error){
+        console.error("Erro no serviço de criar nova transacao");
+        return false;
+    }
+}
+
+const checkIfTransactionTypeMatchesToCategoryType = async (TransactionType, CategoryId) => {
+    try{
+        const getCategory = await prisma.expenseAndRevenueCategories.findUnique({
+            where:{
+                id: CategoryId
+            }
+        })
+        if(getCategory.type === TransactionType){
+            return true;
+        }else{
+            return false;
+        }
+    }catch(error){
+        console.error("Erro ao verificar se o tipo de transacao escolhido confere com o tipo de categoria")
+        return null;
+    }
+}
+
+const getAllTransactionsByAccountId = async (userId, accountId) =>{
+    try{
+        const allTransByAccId = await prisma.transactions.findMany({
+            select:{
+                type: true,
+                amount: true
+            },
+            where:{
+                userId: userId,
+                accountId: accountId
+            }
+        })
+        return allTransByAccId
+    }catch(error){
+        console.error("Erro ao buscar todas as transações vinculadas a esta conta", error);
+        return null
+    }
+}
+
+
+
+//const getAllTranscations = async ()
+module.exports ={
+    checkIfTransactionTypeMatchesToCategoryType,
+    newTransaction,
+    getAllTransactionsByAccountId
+}
+
