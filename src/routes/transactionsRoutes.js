eb45fed5c3bf2e2fa22fddd1ce72b835f894b2bf
Henrique Gouveia <henriquesgo42@gmail.com>
2025-04-13 10:36:09 -0300

    Rota 'get-all-transactions' nao faz sentido por retornar muitos dados de uma vez. Futuramente haverá implementacao de uma funcao que retorna esses dados em partes menores, conforme o usuario for scrollando para baixo numa tela, por exemplo

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const {createTransaction, readMonthTransactions, readUnpaidTransactions, getAllTransactions} = require('../controllers/transactionController');
+const {createTransaction, readMonthTransactions, readUnpaidTransactions} = require('../controllers/transactionController');
 const router = express.Router();
 
 router.post('/create-transaction', createTransaction);
 router.get('/readMonthTransaction', readMonthTransactions);
 router.get('/readUnpaidTransactions', readUnpaidTransactions)
-router.get('/get-all-transactions', getAllTransactions)
 
 module.exports = router;
