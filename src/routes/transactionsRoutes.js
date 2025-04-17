005b0f7ab465df2e9948f118eff75f344fc19af7
Henrique Gouveia <henriquesgo42@gmail.com>
2025-04-17 20:25:51 -0300

    Implementacao do controller 'updateTransaction'

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {createTransaction, readMonthTransactions, readUnpaidTransactions} = require('../controllers/transactionController');
+const {createTransaction, readPaidMonthTransactions, readUnpaidTransactions, updateTransaction} = require('../controllers/transactionController');
 const router = express.Router();
 
 router.post('/create-transaction', createTransaction);
-router.get('/readMonthTransaction', readMonthTransactions);
+router.patch('/update-transaction', updateTransaction)
+router.get('/readPaidMonthTransaction', readPaidMonthTransactions);
 router.get('/readUnpaidTransactions', readUnpaidTransactions)
 
 module.exports = router;
