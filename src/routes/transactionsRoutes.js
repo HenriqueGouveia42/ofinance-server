2d592bfc326ca4ec8983917c4223e42b4509df0f
Henrique Gouveia <henriquesgo42@gmail.com>
2025-05-05 23:28:00 -0300

    Implementacao da rota para deletar uma determinada transacao

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {createTransaction, readPaidMonthTransactions, readUnpaidTransactions, updateTransaction} = require('../controllers/transactionController');
+const {createTransaction, readPaidMonthTransactions, readUnpaidTransactions, updateTransaction, deleteTransaction} = require('../controllers/transactionController');
 const router = express.Router();
 
 router.post('/create-transaction', createTransaction);
+router.delete('/delete-transaction', deleteTransaction);
 router.patch('/update-transaction', updateTransaction)
 router.get('/readPaidMonthTransaction', readPaidMonthTransactions);
 router.get('/readUnpaidTransactions', readUnpaidTransactions)
