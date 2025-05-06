4a0981ebaf107a9b842a8e8adafaf13a83f6e6f6
Henrique Gouveia <henriquesgo42@gmail.com>
2025-05-05 23:19:46 -0300

    Perda do suporte a multiplas moedas

--- prisma/cleanUsersTable.js
+++ prisma/cleanUsersTable.js
@@ -3,9 +3,6 @@ const prisma = new PrismaClient();
 
 async function cleanUserTable() {
     try {
-
-        await prisma.UsersCurrencies.deleteMany();
-        console.log("Tabela UsersCurrencies limpa com sucesso!");
         await prisma.Users.deleteMany(); // Deleta todos os registros da tabela User
         console.log('Tabela Users limpa com sucesso!');
     } catch (error) {
