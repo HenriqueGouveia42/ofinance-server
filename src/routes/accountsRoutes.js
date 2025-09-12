c44e9c192edd08fb75c02eb5eb1f323e89a7dc45
Henrique Gouveia <henriquesgo42@gmail.com>
2025-09-12 11:00:52 -0300

    Nomes mais consistentes

--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createAccountController, updateBalance, getAccounts, deleteAccount, renameAccount} = require('../controllers/accountsController')
+const {createAccountController, updateBalanceController, getAccountsController, deleteAccountController, renameAccountController} = require('../controllers/accountsController')
 
 router.post('/create-account', createAccountController);
-router.get('/get-accounts', getAccounts);
-router.patch('/update-balance', updateBalance);
-router.patch('/rename-account', renameAccount )
-router.delete('/delete-account', deleteAccount);
+router.get('/get-accounts', getAccountsController);
+router.patch('/update-balance', updateBalanceController);
+router.patch('/rename-account', renameAccountController )
+router.delete('/delete-account', deleteAccountController);
 
 module.exports = router;
