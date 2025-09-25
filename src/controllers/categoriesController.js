b47db3e6d4382cdb8e7a7ca8ee3e1f4944c71e0d
Henrique Gouveia <henriquesgo42@gmail.com>
2025-09-25 20:03:31 -0300

    Agora a logica de negocios para deletar uma categoria é de responsabilidade do service, nao mais do controller

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,47 +1,27 @@
-const { createCategory, checkIfCategoryAlreadyExists, updateCategoryName } = require("../services/categoryService");
+const { createCategoryService, checkIfCategoryAlreadyExists, renameCategoryService, deleteCategoryService } = require("../services/categoryService");
 
 const {prisma} = require('../config/prismaClient');
+const AppError = require("../utils/AppError");
 
-const newCategory = async(req, res) =>{
+const createCategory = async(req, res) =>{
     try{
+
         const {name, type} = req.body;
 
-        //Verifica se o tipo é válido
-        if(!['revenue', 'expense'].includes(type)){
-            return res.status(400).json({message: "Tipo de categoria invalido"})
-        }
+        const userId = req.user.id
 
-        //Verifica se o nome é valido
-        if(typeof name !== "string" || !/^[a-zA-Z]/.test(name)){
-            return res.status(400).json({message: "Nome invalido. Deve ser uma string e comecar com uma letra"})
-        }
-
-        const categoryAlreadyExists = await checkIfCategoryAlreadyExists(name, req.user.id, type);
-
-        if(categoryAlreadyExists){
-            return res.status(400).json({message: 'Usuario já tem uma categoria de receita ou despesa com esse nome!'});
-        }
-
-        const toTitleCase = (str) => {
-            return str
-              .toLowerCase()
-              .split(' ')
-              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-              .join(' ');
-        };
-
-        const titleCaseName = toTitleCase(name);
-
-        const category = await createCategory(titleCaseName, type, req.user.id);
-
-        if(!category){
-            return res.status(400).json({message: "Erro na criação da categoria"});
-        }
+        await createCategoryService(name, type, userId)
 
         return res.status(201).json({message: "Categoria criada com sucesso!"});
 
     }catch(error){
-        console.error("Erro ao criar nova categoria");
+
+        console.error("Erro ao criar nova categoria", error);
+
+        if (error instanceof AppError){
+            return res.status(error.statusCode).json({message: error.message})
+        }
+        
         return res.status(500).json({message: "Erro interno ao criar categoria"})
     }
 }
@@ -50,140 +30,47 @@ const renameCategory = async(req, res) =>{
     try{
         const {categoryId, newCategoryName} = req.body;
 
-        const newName = await updateCategoryName(req.user.id, categoryId, newCategoryName);
+        const userId = req.user.id
+
+        const newName = await renameCategoryService(userId, categoryId, newCategoryName);
 
         return newName ? res.status(200).json({message: "Nome da categoria alterado com sucesso"}) : res.status(404).json({message: "Erro ao alterar o nome da categoria"})
     }catch(error){
 
-        console.error("Erro ao mudar o nome da categoria")
-        return res.status(404).json({message: "Erro ao mudar o nome da cateroria: ", error});
+        console.error("Erro ao mudar o nome da categoria", error)
+
+        if (error instanceof AppError){
+            return res.status(error.statusCode).json({message: error.message})
+        }
+
+        return res.status(500).json({message: "Erro interno ao mudar o nome da cateroria"});
     }
 }
 
-
 const deleteCategory = async(req, res) =>{
     try{
-        /*
-            Algoritmo para deletar uma categoria
-            1) Descorbrir todas as transações vinculadas a esta categoria
-            2) Somar todos os balanços, positivos se de receita, negativos se de despesa, de todas transações vinculadas a esta categoria
-            3) Atualizar os novos valores dos balanços das contas
-            4) Deletar todas as transações vinculadas a esta categoria
-            5) Deletar a categoria em si
-        */
+       
         const {categoryId} = req.body;
 
         const userId = req.user.id;
-        
-        const categoryType = await prisma.expenseAndRevenueCategories.findUnique({
-            select:{
-                type: true
-            },
-            where:{
-                userId: userId,
-                id: categoryId
-            }
-        })
 
-        if(!categoryType){
-            return res.status(404).json({message: "Usuario nao possui categoria com esse id"});
-        }
+        const deleteCategory = await deleteCategoryService(categoryId, userId)
 
-        //1)
-        const allTransactionsByCategoryId = await prisma.transactions.findMany({
-            select:{
-                accountId: true,
-                type: true,
-                amount: true,
-            },
-            where:{
-                userId: userId,
-                categoryId: categoryId
-            }
-        })
-
-        if(allTransactionsByCategoryId.length == 0){
-            const delCat = await prisma.expenseAndRevenueCategories.delete({
-                where:{
-                    userId: userId,
-                    id: categoryId 
-                }
-            })
+        if (deleteCategory == "zero_transactions"){
             return res.status(200).json({message:"Não existem transações vinculada a esta categoria. Categoria deletada com sucesso!"})
         }
-
-        //allTranscationsByCategoryId = [
-        //  {accountId: 5, type: "revenue", amount: 1500},
-        //  {accountId: 6, type: "expense", amount: 750},
-        //  {accountId: 5, type: "revenue", amount: 600},
-        //  {accountId: 7, type: "expense", amount: 3500} ...
-        //]
-
-        //2)
-        //Array de objetos do tipo
-        //accountsIdsAndTotalBalance = [
-        // {accountId: 5, totalBalance: -750},
-        // {accountId: 6, totalBalance: -1500},
-        // {accountId: 7, totalBalance: 5600} ...
-        //]  
-        let accountIdsAndTotalBalance = [];
         
-        allTransactionsByCategoryId.forEach(transaction =>{
-
-            let existingAccount = accountIdsAndTotalBalance.find(obj => obj.accountId === transaction.accountId)
-
-            //Já existe uma conta com o id do elemento da iteração atual = transactions.id
-            if(existingAccount){
-                transaction.type === 'revenue' ? 
-                existingAccount.totalBalance += transaction.amount :
-                existingAccount.totalBalance -= transaction.amount
-            }else{
-                accountIdsAndTotalBalance.push({
-                    accountId: transaction.accountId,
-                    totalBalance: transaction.type === 'revenue' ? transaction.amount : -transaction.amount,
-                })
-            }
-        })
-
-        //Os passos 3 e 4 e 5 precisam vir dentro de um bloco 'transaction', pois, caso uma chamada assíncrona falhe, todas as outras feitas anteriormente serão desfeitas, mantendo assim a atomicidade do banco de dados
-        await prisma.$transaction(async (prisma) =>{
-            //3)
-            for(const account of accountIdsAndTotalBalance){
-
-                const updateAccount = await prisma.accounts.update({
-                    where:{
-                        userId: req.user.id,
-                        id: account.accountId
-                    },
-                    data:{
-                        balance: account.totalBalance <= 0
-                        ? {increment: Math.abs(account.totalBalance)}
-                        : {decrement: Math.abs(account.totalBalance)}
-                    }
-                });
-            }
-            //4)
-            const deleteTransactionsByCategory = await prisma.transactions.deleteMany({
-                where:{
-                    userId: req.user.id,
-                    categoryId: categoryId
-                }
-            })
-            //5)
-            const deleteCat = await prisma.expenseAndRevenueCategories.delete({
-                where:{
-                    userId: req.user.id,
-                    id: categoryId
-                }
-            })
-        })
-
         return res.status(200).json({message: "Categoria deletada com sucesso!"});
 
     }catch(error){
         console.error("Erro ao deletar categoria", error);
-        return res.status(404).json({message: "Erro ao deletar categoria"})
+
+        if (error instanceof AppError){
+            return res.status(error.statusCode).json({message: error.message})
+        }
+
+        return res.status(500).json({message: "Erro interno ao deletar categoria"})
     }
 }
 
-module.exports = { newCategory, renameCategory, deleteCategory};
+module.exports = { createCategory, renameCategory, deleteCategory};
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,25 +1,5 @@
 const {prisma} = require('../config/prismaClient');
-const { newCategory } = require('../controllers/categoriesController');
-
-const createCategory = async(name, type, userId) =>{
-    try{
-        const category = await prisma.expenseAndRevenueCategories.create({
-            data:{
-                name,
-                type,
-                userId
-            }
-        })
-        if(category){
-            return category;
-        }else{
-            return null;
-        }
-    }catch(error){
-        console.error("Erro no serviço de criar categoria");
-        return null;
-    }
-}
+const AppError = require('../utils/AppError');
 
 const checkIfCategoryAlreadyExists = async(name, userId, type) =>{
     try{
@@ -41,7 +21,49 @@ const checkIfCategoryAlreadyExists = async(name, userId, type) =>{
     }
 }
 
-//check cache first
+
+const createCategoryService = async(name, type, userId) =>{
+    
+
+    //Verifica se o tipo é válido
+    if(!['revenue', 'expense'].includes(type)){
+        throw new AppError('Tipo de categoria invalido', 400, 'CATEGORY_ERROR')
+    }
+
+    //Verifica se o nome é valido
+    if(typeof name !== "string" || !/^[a-zA-Z]/.test(name)){
+        throw new AppError('Nome da categoria invalido', 400, 'CATEGORY_ERROR')
+    }
+
+    const categoryAlreadyExists = await checkIfCategoryAlreadyExists(name, userId, type);
+
+    if(categoryAlreadyExists){
+        throw new AppError('Essa categoria já existe', 400, 'CATEGORY_ERROR')
+    }
+
+    const toTitleCase = (str) => {
+        return str
+            .toLowerCase()
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+    };
+
+    const titleCaseName = toTitleCase(name);
+
+    const category = await prisma.expenseAndRevenueCategories.create({
+        data:{
+            name: titleCaseName,
+            type: type,
+            userId: userId
+        }
+    })
+
+    if(!category){
+        throw new AppError('Erro ao criar nova categoria', 400, 'CATEGORY_ERROR')
+    }
+}
+
 const checkIfCategoryExists = async(userId, categoryId) => {
     try{
         const checkIfExists = await prisma.expenseAndRevenueCategories.findUnique({
@@ -56,26 +78,27 @@ const checkIfCategoryExists = async(userId, categoryId) => {
     }
 }
 
-const updateCategoryName = async(userId, categoryId, newCategoryName) => {
-    try{
+const renameCategoryService = async(userId, categoryId, newCategoryName) => {
+    
         
-        if(typeof userId ==! "number" || typeof categoryId ==! "number" || typeof newCategoryName ==! "string"){
-            return false;
-        }
-        const newAccName = await prisma.expenseAndRevenueCategories.update({
-            where:{
-                userId: userId,
-                id: categoryId
-            },
-            data:{
-                name: newCategoryName
-            }
-        })
-        return true;
-    }catch(error){
-        console.error("Erro ao atualizar o nome da !!!!!!!!!!!!!!!");
-        return false;
+    if(typeof userId ==! "number" || typeof categoryId ==! "number" || typeof newCategoryName ==! "string"){
+        throw new AppError('Algum parametro da renomeacao esta incorreto', 404, 'CATEGORY_ERROR')
     }
+
+    const newAccName = await prisma.expenseAndRevenueCategories.update({
+        where:{
+            userId: userId,
+            id: categoryId
+        },
+        data:{
+            name: newCategoryName
+        }
+    })
+    
+    if (!newAccName){
+        throw new AppError('Erro ao tentar renomear categoria')
+    }
+    
 }
 
 //check cache first
@@ -98,10 +121,144 @@ const getCategoriesByUserId = async(userId) =>{
     }
 }
 
+const deleteCategoryService = async(categoryId, userId) => {
+
+     /*
+        Algoritmo para deletar uma categoria
+        1) Descobrir todas as transações vinculadas a esta categoria
+        2) Somar todos os balanços, positivos se de receita, negativos se de despesa, de todas transações vinculadas a esta categoria
+        3) Atualizar os novos valores dos balanços das contas
+        4) Deletar todas as transações vinculadas a esta categoria
+        5) Deletar a categoria em si
+    */
+
+    const categoryType = await prisma.expenseAndRevenueCategories.findUnique({
+        select:{
+            type: true
+        },
+        where:{
+            userId: userId,
+            id: categoryId
+        }
+    })
+
+    if (!categoryType){
+        throw new AppError('Erro ao buscar o tipo da categoria a ser deletado', 400, 'CATEGORY_ERROR')
+    }
+
+    //1)
+    const allTransactionsByCategoryId = await prisma.transactions.findMany({
+        select:{
+            accountId: true,
+            type: true,
+            amount: true,
+        },
+        where:{
+            userId: userId,
+            categoryId: categoryId
+        }
+    })
+
+    if (!allTransactionsByCategoryId){
+        throw new AppError('Erro ao buscar todas as transacoes vinculadas a essa categoria', 404, 'CATEGORY_ERROR')
+    }
+
+    if(allTransactionsByCategoryId.length == 0){
+
+        const delCat = await prisma.expenseAndRevenueCategories.delete({
+            where:{
+                userId: userId,
+                id: categoryId 
+            }
+        })
+
+        if (!delCat){
+            throw new AppError('Erro ao deletar as categorias quando nao existem transacoes vinculadas a esta categoria', 404, 'CATEGORY_SERVICE')
+        }
+
+        return "zero_transactions"
+    }
+
+    //1)
+        //allTranscationsByCategoryId = [
+        //  {accountId: 5, type: "revenue", amount: 1500},
+        //  {accountId: 6, type: "expense", amount: 750},
+        //  {accountId: 5, type: "revenue", amount: 600},
+        //  {accountId: 7, type: "expense", amount: 3500} ...
+        //]
+
+
+    let accountIdsAndTotalBalance = [];
+
+    //2)
+        //Array de objetos do tipo
+        //accountsIdsAndTotalBalance = [
+        // {accountId: 5, totalBalance: -750},
+        // {accountId: 6, totalBalance: -1500},
+        // {accountId: 7, totalBalance: 5600} ...
+        //
+    //]
+
+    allTransactionsByCategoryId.forEach(transaction =>{
+
+        let existingAccount = accountIdsAndTotalBalance.find(obj => obj.accountId === transaction.accountId)
+
+        //Já existe uma conta com o id do elemento da iteração atual = transactions.id
+        if(existingAccount){
+            transaction.type === 'revenue' ? 
+            existingAccount.totalBalance += transaction.amount :
+            existingAccount.totalBalance -= transaction.amount
+        }else{
+            accountIdsAndTotalBalance.push({
+                accountId: transaction.accountId,
+                totalBalance: transaction.type === 'revenue' ? transaction.amount : -transaction.amount,
+            })
+        }
+    })
+
+    //Os passos 3 e 4 e 5 precisam vir dentro de um bloco 'transaction', pois, caso uma chamada assíncrona falhe, todas as outras feitas anteriormente serão desfeitas, mantendo assim a atomicidade do banco de dados
+    const cascadeOperationsPrismaTransaction = await prisma.$transaction(async (prisma) =>{
+        //3)
+        for(const account of accountIdsAndTotalBalance){
+
+            const updateAccount = await prisma.accounts.update({
+
+                where:{
+                    userId: userId,
+                    id: account.accountId
+                },
+
+                data:{
+                    balance: account.totalBalance <= 0
+                    ? {increment: Math.abs(account.totalBalance)}
+                    : {decrement: Math.abs(account.totalBalance)}
+                }
+            });
+        }
+        //4)
+        const deleteTransactionsByCategory = await prisma.transactions.deleteMany({
+            where:{
+                userId: userId,
+                categoryId: categoryId
+            }
+        })
+
+        //5)
+        const deleteCat = await prisma.expenseAndRevenueCategories.delete({
+            where:{
+                userId: userId,
+                id: categoryId
+            }
+        })
+    })
+
+}
+
 module.exports ={
-    createCategory,
+    createCategoryService,
     checkIfCategoryAlreadyExists,
-    updateCategoryName,
+    deleteCategoryService,
+    renameCategoryService,
     checkIfCategoryExists,
     getCategoriesByUserId 
 }
