000a345fa807d903bfb045256510ee98c0e742d1
Henrique Gouveia <henriquesgo42@gmail.com>
2025-09-25 14:56:36 -0300

    Aumento na robustez da verificacao do token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { errorMonitor } = require("nodemailer/lib/xoauth2");
 
 // O authMiddleware verifica se o token JWT enviado via cookie é valido
 // localhost:5000/transaction -> authMiddleware -> transactionRoutes
@@ -7,20 +8,22 @@ const authMiddleware = (req, res, next) =>{
     
     const token = req.cookies.access_token;
 
-    if(!token){
+    //console.log("Token que chegou no authMiddleware: " + token)
+
+    if(!token || token.trim() === ''){
         return res.status(401).json({message: "Acesso negado. Token não fornecido"});
     }
 
-    const validToken = jwt.verify(token, process.env.JWT_SECRET);
-    
-    if(!validToken){
-        return res.status(403).json({message: "Token invalido ou expirado"});
+    try{
+        const validToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = validToken; //Anexa os dados do usuario ao objeto de requisição
+        next(); 
+    }catch(err){
+        console.error("Erro na verificação do token JWT: ", err.message)
+        return res.status(403).json({message: "Token inválido ou expirado"})
     }
-
-    req.user = validToken; //Anexa os dados do usuario ao objeto de requisição
-    
+ 
     //Chama o proximo middleware ou controller na cadeia de execucao
-    next(); 
 }
 
 
