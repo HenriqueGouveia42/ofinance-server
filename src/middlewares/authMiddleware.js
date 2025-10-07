9985e88bc41a3913bddf051d3bf69f85b3ec66c3
Henrique Gouveia <henriquesgo42@gmail.com>
2025-10-07 11:06:44 -0300

    Apaguei comentario de debug

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -8,8 +8,6 @@ const authMiddleware = (req, res, next) =>{
     
     const token = req.cookies.access_token;
 
-    console.log(token)
-
     //console.log("Token que chegou no authMiddleware: " + token)
 
     if(!token || token.trim() === ''){
